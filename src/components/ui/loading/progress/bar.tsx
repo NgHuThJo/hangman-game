e9e684e6390711d5a453b52bdd6dacf453d34340
John Doe <nghuthjo@gmail.com>
2024-11-27 14:34:03 +0100

    Commented out linter in workflow

--- src/components/ui/loading/progress/bar.tsx
+++ src/components/ui/loading/progress/bar.tsx
@@ -3,5 +3,7 @@ import { useProgress } from "#frontend/hooks/use-upload-download-progress";
 export function ProgressBar() {
   const { downloadProgress, uploadProgress } = useProgress();
 
-  return <progress value={uploadProgress + downloadProgress} max={100}></progress>;
+  return (
+    <progress value={uploadProgress + downloadProgress} max={100}></progress>
+  );
 }
